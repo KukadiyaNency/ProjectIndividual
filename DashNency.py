5ea1131b1b5b784e24c3085543cf7b2dc44a7644
KukadiyaNency <163130955+KukadiyaNency@users.noreply.github.com>
2024-05-04 11:51:43 +0100

    Deleting file because it was not caching DashNency.py

--- DashNency.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import plotly.express as px
-import pandas as pd
-import os
-import warnings
-warnings.filterwarnings('ignore')
-st.set_page_config(page_title='Health Analysis', page_icon='bar_chart:', layout='wide')
-
-st.write("Hello streamlit!")
